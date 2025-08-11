b2021afe6fc10331981f41e0fefb8121eb1196c4
soares <soaresgithub@gmail.com>
2025-08-11 17:16:17 -0300

    Add files via upload

---
+++ aula1/index.php
@@ -0,0 +1,88 @@
+<?php
+    $nome = "soares";
+    $idade = 15;
+    echo "Meu nome é ".$nome.", e tenho ".$idade." anos.";
+    echo "<hr>";
+    echo "<center><h2>SENAI - DEVELOPER</h2></center>";
+    echo "<hr>";
+    $valor1 = 8;
+    $valor2 = 11;
+    echo "A soma dos valores foi:".$valor1+$valor2."<br>";
+    echo "A subtração dos valores foi:".$valor1-$valor2."<br>";
+    echo "A multiplicação dos valores foi:".$valor1*$valor2."<br>";
+    $result = $valor1/$valor2;
+    $numformatado = number_format($result, 2, ',', '.');
+    echo "A divisão dos valores foi:".$numformatado;
+    echo "<hr>";
+    $datahora = date('d/m/Y H:i:s');
+    echo "Data atual: ".$datahora;
+    echo "<hr>";
+    $data1 = new Datetime('2025-01-01');
+    //Data: 2025-01-01 22:30:23 = Y-m-d H:i:s
+    $data2 = new Datetime('2025-08-06');
+    $intervalo = $data1 ->diff($data2);
+    echo "A diferença em anos:".$intervalo->y."<br>";
+    echo "A diferença em meses: ".$intervalo->m."<br>";
+    echo "A difenrença em dias: ".$intervalo->days."<br>";
+    $horas = $intervalo->days*24;
+    echo "A diferença em horas: ".$horas;
+    echo "<hr>";
+    echo "<h2>Função Condicional - IF</h2>";
+    $a = 3;
+    $b = 5;
+    if ($a > $b) {
+        echo "A é maior que B!";
+    } else {
+        echo "B é maior que A!";
+    }
+    echo "<br>Saiu do IF";
+echo "<br>";
+$n1 = 3;
+$n2 = 4;
+$n3 = 8;
+if ($n1 > $n2 && $n1 > $n3) {
+    echo "A variavel ".$n1. "é o valor maior!";
+}
+elseif ($n2 > $n1 && $n2 > $n3) {
+    echo "A variavel ".$n2. "é o valor maior!";
+}
+  else {
+    echo "A variavel ".$n3. "é o valor maior!";
+  }  
+  echo "<hr>";
+  echo "<center><h2>NUMEROS RANDOMICOS</h2></center>";
+$num = rand (1, 10);
+ $meunum = 6;
+ if ($num == $meunum) {
+    echo "o valor da sua variavel ".$meunum. ". o numero sorteado foi";
+ }
+ else {
+    echo "valor da sua variavel ";
+ }
+ echo "<hr>";
+ for ($i=0; $i < 10; $i++) {
+  echo "passagem do laço for nº: ".$i." <br>";
+ }
+echo "<h2>Tabuada</h2>";
+$numtab = 5;
+for ($i=1; $i <= 10 ; $i++) {
+    echo $numtab." X ".$i." = ".$numtab*$i. "<br>";
+}
+echo "<hr>";
+echo s"<h2>laço WHILE:</h2>";
+$x = 1;
+while ($x <6) {
+echo "<h2>laço nº:  ".$x." <br>";
+}
+echo "<h2>laço nº:  ".$x." <br>";   
+$a = 0;
+while ($a < 100) {
+    echo $a. "<br>";
+    $a+=10;
+}
+echo "<hr>";
+$cores = array("azul", "marrom","bege","amarelo","branco");
+foreach ($cores as $cor) {
+    echo $cor. "<br>";
+}
+ 
